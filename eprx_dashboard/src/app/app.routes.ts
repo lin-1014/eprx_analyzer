a005a315f2568c4288af1ffe1acd753b0f8c788e
linn-1014 <prmotoprlinn1014@gmail.com>
2025-02-16 17:02:18 +0800

    feat: add echarts package and configure routing for heatmap component

--- eprx_dashboard/src/app/app.routes.ts
+++ eprx_dashboard/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HeatmapComponent } from './pages/heatmap/heatmap.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'heatmap', component: HeatmapComponent },
+    { path: '', component: HeatmapComponent },
+
+];
